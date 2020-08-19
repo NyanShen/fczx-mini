a6b06bf3d76a5bd7974a3ab9634acd89627bc346
沈艳美 <3354509516@qq.com>
2020-08-19 10:51:28 +0800

    update search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -20,6 +20,9 @@ const Search = () => {
     setClear(false);
     setSearchValue("");
   }
+  const handleSearchItemClick = (event) => {
+    console.log(event)
+  }
   return (
     <View className="search">
       <NavBar title="搜索" back={true} />
@@ -34,13 +37,27 @@ const Search = () => {
         </View>
         <Text className="search-cancel">取消</Text>
       </View>
-      <View className="search-history">
+      <View className="search-record search-history">
         <View className="search-header clearfix">
           <Text className="title">历史搜索</Text>
-          <Text className="iconfont">x</Text>
+          <Text className="iconfont iconclear1"></Text>
         </View>
-        <View className="history-list">
-          <Text>吾悦广场</Text>
+        <View className="search-list clearfix" onClick={handleSearchItemClick}>
+          <Text className="item">吾悦广场</Text>
+          <Text className="item">水运楼</Text>
+        </View>
+      </View>
+      <View className="search-record search-hot">
+        <View className="search-header clearfix">
+          <Text className="title">热门搜索</Text>
+        </View>
+        <View className="search-list clearfix">
+          <Text className="item">吾悦广场</Text>
+          <Text className="item">广场</Text>
+          <Text className="item">万达广场</Text>
+          <Text className="item">万达场</Text>
+          <Text className="item">万达广</Text>
+          <Text className="item">万广场</Text>
         </View>
       </View>
     </View>
