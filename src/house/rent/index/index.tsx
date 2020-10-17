08fdb8afd7941dad14d9a78dc5d1f9930b66a4b8
沈艳美 <3354509516@qq.com>
2020-10-17 11:21:24 +0800

    update rent index

--- src/house/rent/index/index.tsx
+++ src/house/rent/index/index.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { View, ScrollView, Text } from '@tarojs/components'
+import React, { useState } from 'react'
+import { View, ScrollView, Text, Image, Map } from '@tarojs/components'
 
 import NavBar from '@components/navbar'
 import useNavData from '@hooks/useNavData'
 import '@styles/common/bottom-bar.scss'
 import './index.scss'
+
 const RentIndex = () => {
 
+    const initMarker = [
+        {
+            latitude: 32.068105,
+            longitude: 112.139804,
+            width: 30,
+            height: 30,
+            iconPath: 'http://192.168.2.248/assets/mini/location.png',
+            callout: {
+                content: '小区名称',
+                color: '#fff',
+                fontSize: 14,
+                borderWidth: 2,
+                borderRadius: 5,
+                borderColor: '#11a43c',
+                bgColor: '#11a43c',
+                padding: 5,
+                display: 'ALWAYS',
+                textAlign: 'center'
+            }
+        }
+    ]
+
     const { contentHeight } = useNavData()
+    const [rentData] = useState<any>({ rentMarker: initMarker })
+
     return (
         <View className="rent">
             <NavBar title="租房" back={true}></NavBar>
@@ -15,8 +40,8 @@ const RentIndex = () => {
                 <View className="house-album">
 
                 </View>
-                <View className="rent-content">
-                    <View className="rent-content-header">
+                <View className="rent-item">
+                    <View className="rent-item-header">
                         <View className="title mb16">
                             全市低3千1平中豪旁边天然气入户层高全市低3千1平中豪旁边天然气
                         </View>
@@ -28,7 +53,9 @@ const RentIndex = () => {
                             <Text className="tags-item sale-status-2">整租</Text>
                         </View>
                     </View>
-                    <View className="rent-content-info">
+                </View>
+                <View className="rent-item">
+                    <View className="rent-item-info">
                         <View>
                             <Text className="price">2200</Text>
                             <Text className="price-unit">元/月</Text>
@@ -59,14 +86,99 @@ const RentIndex = () => {
                                 <Text className="label">电梯</Text>
                                 <Text className="value">有</Text>
                             </View>
+                            <View className="info-item">
+                                <Text className="label">要求</Text>
+                                <Text className="value">男女不限</Text>
+                            </View>
                         </View>
+                        <View className="facility-content">
+                            <View className="label">配套设施</View>
+                            <View className="facility-list">
+                                <View className="facility-item">
+                                    <Text className="iconfont iconcheck"></Text>
+                                    <Text>床</Text>
+                                </View>
+                                <View className="facility-item">
+                                    <Text className="iconfont iconcheck"></Text>
+                                    <Text>洗衣机</Text>
+                                </View>
+                                <View className="facility-item">
+                                    <Text className="iconfont iconcheck"></Text>
+                                    <Text>宽带</Text>
+                                </View>
+                                <View className="facility-item">
+                                    <Text className="iconfont iconcheck"></Text>
+                                    <Text>空调</Text>
+                                </View>
+                                <View className="facility-item">
+                                    <Text className="iconfont iconcheck"></Text>
+                                    <Text>热水器</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="rent-tip">
+                            <Text>郑重提示：请您在签订合同之前，切勿支付任何形式的费用，以免上当受骗。</Text>
+                        </View>
+                    </View>
+                </View>
+                <View className="rent-item">
+                    <View className="rent-item-info">
+                        <View className="header">
+                            <Text className="title">房源描述</Text>
+                        </View>
+                        <View className="rent-info-consultant">
+                            <View className="user-photo">
+                                <Image src=""></Image>
+                            </View>
+                            <View>
+                                <View>苏家园</View>
+                                <View className="small-desc">置业顾问</View>
+                            </View>
+                        </View>
+                        <View className="rent-desc">
+                            房子位于长虹路毛纺小区站台背后，二楼大两室，家具家电齐全拎包入住，独栋独院，户型方正，南北通透，安全有保证
+                        </View>
+                    </View>
+                </View>
+                <View className="rent-item">
+                    <View className="rent-item-info">
+                        <View className="plot-header">
+                            <View className="title">
+                                <Text>小区详情：</Text>
+                                <Text className="link">中豪旁边天然气小区</Text>
+                            </View>
+                            <View className="more">
+                                <Text className="iconfont iconarrow-right-bold"></Text>
+                            </View>
+                        </View>
+                        <View className="rent-map">
+                            <Map
+                                id="rentMap"
+                                className="map"
+                                latitude={32.068105}
+                                longitude={112.139804}
+                                enableZoom={false}
+                                markers={rentData.rentMarker}
+                            >
+                            </Map>
+                        </View>
+                    </View>
+                </View>
+                <View className="rent-item">
+                    <View className="small-desc">
+                        免责声明：房源信息由网站用户提供，其真实性、合法性由信息提供者负责，最终以政府部门登记备案为准。本网站不声明或保证内容之正确性和可靠性，租赁该房屋时，请谨慎核查。
                     </View>
                 </View>
             </ScrollView>
             <View className="bottom-bar">
                 <View className="bar-item">
-                    <Text className="iconfont iconhome"></Text>
-                    <Text>首页</Text>
+                    <View className="user-photo">
+                        <Image src=""></Image>
+                    </View>
+                    <View>
+                        <View>苏家园</View>
+                        <View className="small-desc">置业顾问</View>
+                    </View>
                 </View>
                 <View className="bar-item-btn">
                     <Text className="btn btn-yellow btn-bar">在线咨询</Text>
