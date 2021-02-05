c90e141bf8be7279bad90e8497b79400f02d02ba
沈艳美 <3354509516@qq.com>
2021-02-05 17:24:25 +0800

    update album add video list

--- src/house/new/album/index.tsx
+++ src/house/new/album/index.tsx
@@ -1,15 +1,16 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { getCurrentInstance } from '@tarojs/taro'
-import { View, ScrollView, Text, Image, Swiper, SwiperItem } from '@tarojs/components'
+import Taro, { getCurrentInstance, previewImage } from '@tarojs/taro'
+import { View, ScrollView, Text, Image } from '@tarojs/components'
 import classnames from 'classnames'
+import map from 'lodash/map'
 
 import api from '@services/api'
 import app from '@services/request'
 import useNavData from '@hooks/useNavData'
+import { toUrlParam } from '@utils/urlHandler'
 import './index.scss'
 
 interface IAlbumSwiper {
-    open: boolean
     albumId: string
     imageIndex: number
     swiperIndex: number
@@ -17,7 +18,6 @@ interface IAlbumSwiper {
 }
 
 const INIT_ALBUM_SWIPER = {
-    open: false,
     albumId: '',
     imageIndex: 0,
     swiperIndex: 0,
@@ -59,45 +59,25 @@ const AlbumList = () => {
     }
 
     const handleImageClick = (albumItem: any, imageIndex: number) => {
-        const swiperItem = findAlbumById(albumItem.id)
-        setAlbumSwiper({
-            open: true,
-            albumId: swiperItem.id,
-            imageIndex: imageIndex,
-            swiperIndex: swiperItem.indexBefore + imageIndex,
-            imageLength: swiperItem.length
+        if (albumItem.type == '2') {
+            const paramString = toUrlParam({
+                video: JSON.stringify(albumItem.fangHouseImage[imageIndex])
+            })
+            Taro.navigateTo({
+                url: `/house/new/video/index${paramString}`
+            })
+            return
+        }
+        previewImage({
+            current: albumItem.fangHouseImage[imageIndex].image_path,
+            urls: map(albumItem.fangHouseImage, 'image_path')
         })
+        
         if (currentView === `view_${albumItem.id}`) {
             return
         }
         setCurrentView(`view_${albumItem.id}`)
     }
-
-    const onSwiperChange = (event) => {
-        let swiperIndex = event.detail.current;
-        let currentItem = event.detail.currentItemId.split(',');
-        let albumId = currentItem[0];
-        let imageIndex = parseInt(currentItem[1]);
-        let imageLength = findAlbumById(albumId).length
-        setAlbumSwiper({
-            ...albumSwiper,
-            albumId,
-            imageIndex,
-            swiperIndex,
-            imageLength
-        })
-        if (currentView === `view_${albumId}`) {
-            return
-        }
-        setCurrentView(`view_${albumId}`)
-    }
-
-    const handleCloseAlbum = useCallback(() => {
-        setAlbumSwiper({
-            ...albumSwiper,
-            open: false
-        })
-    }, [albumSwiper.open])
 
     const findAlbumById = useCallback((albumId: string) => {
         let indexBefore = 0;
@@ -155,6 +135,7 @@ const AlbumList = () => {
                                                 className="album-list-item"
                                                 onClick={() => handleImageClick(item, imageIndex)}
                                             >
+                                                {item.type == '2' && <Text className="icon-vedio"></Text>}
                                                 <Image src={imageItem.image_path} mode="aspectFill"></Image>
                                             </View>
                                         ))
@@ -165,58 +146,6 @@ const AlbumList = () => {
                     }
                 </ScrollView>
             </View>
-
-            {
-                albumSwiper.open &&
-                <View className="album-swiper" style={{ top: 0 }}>
-                    <View className="album-swiper-header">
-                        <View className="album-count">
-                            <Text>{albumSwiper.imageIndex + 1}/{albumSwiper.imageLength}</Text>
-                        </View>
-                        <View className="iconfont iconclose" onClick={handleCloseAlbum}></View>
-                    </View>
-                    <View className="album-swiper-content">
-                        <Swiper
-                            circular
-                            style={{ height: contentHeight - 80 }}
-                            current={albumSwiper.swiperIndex}
-                            onChange={onSwiperChange}
-                        >
-                            {
-                                albumData.length > 0 &&
-                                albumData.map((albumItem: any) => {
-                                    return albumItem.fangHouseImage.map((imageItem: any, imageIndex: number) => {
-                                        return (
-                                            <SwiperItem key={imageIndex} itemId={`${albumItem.id},${imageIndex}`}>
-                                                <Image className="swiper-image" src={imageItem.image_path} mode='widthFix'></Image>
-                                                <View className="swiper-text">
-                                                    <Text>{albumItem.name}</Text>
-                                                </View>
-                                            </SwiperItem>
-                                        )
-                                    })
-                                })
-                            }
-                        </Swiper>
-                    </View>
-                    <View className="album-swiper-footer">
-                        <ScrollView className="album-tabs" scrollX>
-                            {
-                                albumData.length > 0 &&
-                                albumData.map((item: any, index: number) => (
-                                    <View
-                                        key={index}
-                                        onClick={() => handleTabClick(item, true)}
-                                        className={classnames('tab-item', currentView === `view_${item.id}` && 'actived')}>
-                                        <Text>{item.name}</Text>
-                                    </View>
-                                ))
-                            }
-                        </ScrollView>
-                    </View>
-                </View>
-            }
-
         </View>
     )
 }
